818c51865ad9012da40453b72c736b04e2094f45
Gaston Mancini <gastonmancini@gmail.com>
2021-06-09 20:08:34 -0300

    Implement logout

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 function Header() {
 
@@ -12,12 +13,21 @@ function Header() {
         })();
     }, []);
 
+    async function Logout() {
+      await axios.post('logout', {});
+    }
+
     return (
         <nav className="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
             <a className="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="#">React for go auth api sample</a>
             <ul className="my-2 my-md-0 mr-md-3">
-                <a className="p-2 text-white text-decoration-none" href="#">{user?.firstName}</a>
-                <a className="p-2 text-white text-decoration-none" href="#">Sign out</a>
+              <Link to={"/profile"} className="p-2 text-white text-decoration-none" href="#">
+                {user?.firstName}
+              </Link>
+              <Link to={"/login"} className="p-2 text-white text-decoration-none"
+                    onClick={Logout}>
+                    Sign out
+              </Link>
             </ul>
         </nav>
     );
