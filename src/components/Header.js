0e09d21d400027549a8d9396a51c49a9f087597b
Gaston Mancini <gastonmancini@gmail.com>
2021-06-09 17:05:29 -0300

    Create header and nav components

---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header className="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
+      <a className="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="#">React for go auth api sample</a>
+      <button className="navbar-toggler position-absolute d-md-none collapsed"
+        type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu"
+        aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <ul className="navbar-nav px-3">
+        <li className="nav-item text-nowrap">
+          <a className="nav-link" href="#">Sign out</a>
+        </li>
+      </ul>
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/components/Nav.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+function Nav() {
+    return (
+        <nav id="sidebarMenu" className="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
+            <div className="position-sticky pt-3">
+                <ul className="nav flex-column">
+                    <li className="nav-item">
+                        <a className="nav-link active" aria-current="page" href="#">
+                            <span data-feather="home"></span>
+                    Dashboard
+                    </a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">
+                            <span data-feather="users"></span>
+                    Users
+                    </a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    );
+}
+
+export default Nav;
