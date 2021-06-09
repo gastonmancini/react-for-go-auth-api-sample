22b86d508c88f0d4302be14d036c3155561ad68f
Gaston Mancini <gastonmancini@gmail.com>
2021-06-09 20:01:58 -0300

    Show user name in the header nav bar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,26 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 
 function Header() {
-  return (
-    <header className="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
-      <a className="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="#">React for go auth api sample</a>
-      <button className="navbar-toggler position-absolute d-md-none collapsed"
-        type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu"
-        aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <ul className="navbar-nav px-3">
-        <li className="nav-item text-nowrap">
-          <a className="nav-link" href="#">Sign out</a>
-        </li>
-      </ul>
-    </header>
-  );
+
+    const [user, setUser] = useState({ firstName: '' });
+
+    useEffect(() => {
+        (async () => {
+            const { data } = await axios.get('http://localhost:8000/api/me', { withCredentials: true });
+            setUser(data);
+        })();
+    }, []);
+
+    return (
+        <nav className="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
+            <a className="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="#">React for go auth api sample</a>
+            <ul className="my-2 my-md-0 mr-md-3">
+                <a className="p-2 text-white text-decoration-none" href="#">{user?.firstName}</a>
+                <a className="p-2 text-white text-decoration-none" href="#">Sign out</a>
+            </ul>
+        </nav>
+    );
 }
 
 export default Header;
