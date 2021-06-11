da90c140a20fcd12fb2613861f3936afb59e75e7
Gaston Mancini <gastonmancini@gmail.com>
2021-06-11 11:11:54 -0300

    Change header title

--- src/components/Header.js
+++ src/components/Header.js
@@ -27,7 +27,7 @@ function Header() {
 
     return (
         <nav className="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
-            <a className="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="#">React for go auth api sample</a>
+            <a className="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="#">React</a>
             <ul className="my-2 my-md-0 mr-md-3">
               <Link to={"/profile"} className="p-2 text-white text-decoration-none" href="#">
                 {user.name}
